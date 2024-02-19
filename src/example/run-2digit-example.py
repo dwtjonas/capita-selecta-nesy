c9d811fbd427d21429f5c9cb07e41343d9a7e04c
Jonas Dewit <jonas.dewit1@student.kuleuven.be>
2024-02-19 16:27:55 +0100

    testing

--- src/example/run-2digit-example.py
+++ src/example/run-2digit-example.py
@@ -6,17 +6,21 @@ from nesy.semantics import GodelTNorm, LukasieviczTNorm, SumProductSemiring
 import torch
 import pytorch_lightning as pl
 
-task_train = AdditionTask(n_classes=2)
-task_test = AdditionTask(n_classes=2, train=False)
+for i in range(4,5 ):
+    print(i)
+    print('\n')
+    task_train = AdditionTask(n_classes=i)
+    task_test = AdditionTask(n_classes=i, train=False)
 
-neural_predicates = torch.nn.ModuleDict({"digit": MNISTEncoder(task_train.n_classes)})
+    neural_predicates = torch.nn.ModuleDict({"digit": MNISTEncoder(task_train.n_classes)})
 
-model = NeSyModel(program=task_train.program,
-                  logic_engine=ForwardChaining(),
-                  neural_predicates=neural_predicates,
-                  label_semantics=SumProductSemiring())
+    model = NeSyModel(program=task_train.program,
+                    logic_engine=ForwardChaining(),
+                    neural_predicates=neural_predicates,
+                    label_semantics=GodelTNorm())
 
-trainer = pl.Trainer(max_epochs=1)
-trainer.fit(model=model,
-            train_dataloaders=task_train.dataloader(batch_size=2),
-            val_dataloaders=task_test.dataloader(batch_size=2))
+    trainer = pl.Trainer(max_epochs=1)
+    trainer.fit(model=model,
+                train_dataloaders=task_train.dataloader(batch_size=3),
+                val_dataloaders=task_test.dataloader(batch_size=3))
+    print("run ended\n")
