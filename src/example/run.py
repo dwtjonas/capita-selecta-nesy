c9a4e98004a3663cf66172e58247559c3517acf5
Jonas Dewit <jonas.dewit1@student.kuleuven.be>
2024-02-19 23:39:39 +0100

    naam veranderd

--- src/example/run.py
+++ src/example/run.py
@@ -1,13 +1,15 @@
 from nesy.model import NeSyModel, MNISTEncoder
 from dataset import AdditionTask
 from nesy.logic import ForwardChaining
-from nesy.semantics import GodelTNorm, LukasieviczTNorm, SumProductSemiring
+from nesy.semantics import GodelTNorm, LukasieviczTNorm, ProductTNorm, SumProductSemiring
 
 import torch
 import pytorch_lightning as pl
 
-n_classes = 3
-NUMBER_OF_DIGITS = 3
+n_classes = 2
+NUMBER_OF_DIGITS = 2
+num_workers = 0
+batch_size = 2
 
 task_train = AdditionTask(n=NUMBER_OF_DIGITS, n_classes=n_classes)
 task_test = AdditionTask(n=NUMBER_OF_DIGITS, n_classes=n_classes, train=False)
@@ -17,9 +19,9 @@ neural_predicates = torch.nn.ModuleDict({"digit": MNISTEncoder(task_train.n_clas
 model = NeSyModel(program=task_train.program,
                 logic_engine=ForwardChaining(),
                 neural_predicates=neural_predicates,
-                label_semantics=SumProductSemiring())
+                label_semantics=ProductTNorm())
 
 trainer = pl.Trainer(max_epochs=1)
 trainer.fit(model=model,
-            train_dataloaders=task_train.dataloader(batch_size=2),
-            val_dataloaders=task_test.dataloader(batch_size=2))
+            train_dataloaders=task_train.dataloader(batch_size=batch_size, num_workers=num_workers),
+            val_dataloaders=task_test.dataloader(batch_size=batch_size, num_workers=num_workers))
