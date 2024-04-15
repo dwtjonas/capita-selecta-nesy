cc17eed935a04d9c1e11a45bc20513c3c47c07da
Jonas Dewit <jonas.dewit1@student.kuleuven.be>
2024-04-15 23:50:56 +0200

    final changes

--- example/dataset.py
+++ example/dataset.py
@@ -2,7 +2,7 @@ import string
 
 import numpy as np
 import operator
-
+import random
 from nesy.parser import parse_program, parse_clause
 
 import torch
@@ -170,6 +170,13 @@ def generate_queries(n_digits):
     res += "{})."
     return res
 
+def generate_queries_ops(n_digits):
+    res = "operation("
+    for i in range(n_digits):
+        res += f"tensor(images, {i}), "
+    res += "{})."
+    return res
+
 class AdditionTask(Dataset):
 
     def __init__(self, n=2, train=True, n_classes=4, nr_examples=None):
@@ -245,6 +252,94 @@ class AdditionTask(Dataset):
             queries = [parse_program(generate_queries(self.num_digits).format(z))[0].term
                        for z in range(self.n_classes * 2 - 1)]
             tensor_sources = {"images": images}
+            return tensor_sources, queries, target
+
+    def dataloader(self, batch_size=2, shuffle=None, num_workers=0):
+        if shuffle is None:
+            shuffle = self.train
+
+        return DataLoader(self, batch_size=batch_size, shuffle=shuffle, collate_fn=custom_collate,
+                          num_workers=num_workers)
+
+    def __len__(self):
+        return self.nr_examples
+class NumOpsTask(Dataset):
+
+    def __init__(self, n=2, train=True, n_classes=10, nr_examples=None, num_operators=2):
+        assert n == 2, "Only n=2 is supported at the moment"
+        self.train = train
+        self.operator_images = []
+        self.operator_targets = []
+
+        # We iterate over the MNIST dataset to apply the transform
+        self.original_images = []
+        self.original_targets = []
+        for x,y in  MNIST('data/MNIST/', train=train, download=True, transform=transform):
+            if y < n_classes:
+                self.original_images.append(x)
+                self.original_targets.append(y)
+            if y < num_operators:
+                self.operator_images.append(x)
+                self.operator_targets.append(y)
+
+        self.original_images = torch.stack(self.original_images)
+        self.original_targets = torch.tensor(self.original_targets)
+        self.n_classes = n_classes
+        self.num_digits = n
+        self.len_operator = len(self.operator_images)
+        program_string = "operation(Op,X,Y,Z) :- operator(Op,N1), digit(X,N2), digit(Y,N3), op(N1,N2,N3,Z).\n"
+        program_string += "\n".join(
+            [f"op(1,{x}, {y}, {x * y})." for x in range(self.n_classes) for y in range(self.n_classes)])
+        program_string += "\n".join(
+            [f"op(0,{x}, {y}, {x + y})." for x in range(self.n_classes) for y in range(self.n_classes)])
+        program_string += "\n"
+        program_string += "\n".join(
+            [f"nn(digit, tensor(images, {x}), {y}) :: digit(tensor(images, {x}),{y})." for x, y in
+             product(range(1, self.num_digits + 1), range(self.n_classes))])
+        program_string += "\n".join(
+            [f"nn(operator, tensor(images, 0), {y}) :: operator(tensor(images, 0),{y})." for  y in
+             range(num_operators)])
+        self.program = parse_program(program_string)
+
+        if nr_examples is not None:
+            if nr_examples > self.nr_examples:
+                raise ValueError('nr_examples exceeds to number of available examples in this dataset')
+            else:
+                self.nr_examples = nr_examples
+        else:
+            self.nr_examples = len(self.original_images) // self.num_digits
+
+    def __getitem__(self, index):
+        images = self.original_images[index * self.num_digits: (index + 1) * self.num_digits ]
+        targets = self.original_targets[index * self.num_digits: (index + 1) * self.num_digits]
+        random_index = random.randint(0, self.len_operator-1)
+        operator_image = self.operator_images[random_index]
+        operator_target = self.operator_targets[random_index]
+
+        symbol = operator_target # 0 = add, 1 = mult
+        if symbol == torch.tensor(1):
+            target = targets[0]
+            for t in targets[1:]:
+                target *= t
+        else:
+            target = sum(targets)
+
+        if self.train:
+            # In MNIST Addition, training queries for a single pair of images check for a given sum (i.e. the target)
+            # Therefore, we have a List[Term], each element of the list correspond to a single pair of images
+
+            query = parse_program(generate_queries_ops(self.num_digits + 1).format(target))[0].term
+            tensor_sources = {"images": images, "operator": operator_image}
+            return tensor_sources, query, torch.tensor([1.0])
+        else:
+            # In MNIST Addition, testing queries for a single pair of images check for all possible sums.
+            # In this way, we can compute the most probable sum.
+            # Therefore, we have a List[List[Term]], each element of the outer list correspond to a single pair of
+            # images. Each element of the inner list correspond to a possible sum.
+
+            queries = [parse_program(generate_queries_ops(self.num_digits + 1).format(target))[0].term
+                       for z in range(self.n_classes * 2 - 1)]
+            tensor_sources = {"images": images, "operator": operator_image}
             return tensor_sources, queries, target
 
     def dataloader(self, batch_size=2, shuffle=None, num_workers=0):
